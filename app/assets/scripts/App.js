b30d6187785a129733b80d9112299bf27017b6d4
amirda <121286048+amirdaGankhuyag@users.noreply.github.com>
2023-12-17 12:49:20 +0800

    Desktop bolon mobile header style-iig buren hiij duuslaa

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,8 @@
-// import '../styles/styles.css';
+import '../styles/styles.css';
+import MobileMenu from './modules/MobileMenu';
 
-// if(module.hot) {
-//     module.hot.accept();
-// }
+let mobileMenu = new MobileMenu();
 
-
-console.log("ydag sd wee chi JS lalr min");
+if(module.hot) {
+    module.hot.accept();
+}
